cd5e951308d4b8274531bade7b5dd974999f3d94
Mark Santoro <mark_santoro@comcast.net>
2024-11-19 15:58:44 -0800

    styling the flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,29 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
    @State var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland",
-                    "Spain","UK","Ukraine","US"].shuffled()
+                    "Spain","UK","Russia","US"].shuffled()
     @State var correctAnswer = Int.random(in: 0...2)
      
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    
+          
     var body: some View {
         ZStack{
-            Color.blue
+            LinearGradient(colors: [.blue,.gray],startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30){
                 VStack{
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                        
                 }
-                
+                          
                 ForEach(0..<3) {number in
                     Button{
                         flagTapped(number)
                     }label:{
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                     
                 }
